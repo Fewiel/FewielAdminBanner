9e57c3e90ee45b463cafa845612d660d2604af68
Bojan Rajh <b.rajh@shopware.com>
2025-09-26 10:32:42 +0200

    fix: sidebar overflow

--- src/Resources/app/administration/src/extension/sw-desktop/index.js
+++ src/Resources/app/administration/src/extension/sw-desktop/index.js
@@ -1,8 +1,8 @@
-import template from './sw-admin.html.twig';
+import template from './sw-desktop.html.twig';
 
 // Ensure the banner component is registered
 import '../../component/fewiel-admin-banner';
 
-Shopware.Component.override('sw-admin', {
+Shopware.Component.override('sw-desktop', {
     template,
 });
